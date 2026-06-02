4c9a64356646bba25ec7475b813afbb67b856e5c
Oli Wilkins <okwilkins@googlemail.com>
2026-06-02 20:01:17 +0100

    Add solution for vector-subtract

---
+++ study-plans/cuda-basics/vector-subtract/vector-subtract.cu
@@ -0,0 +1,18 @@
+#include <cuda_runtime.h>
+
+__global__ void vector_sub(const float* A, const float* B, float* C, int N) {
+    const unsigned int idx{blockDim.x * blockIdx.x + threadIdx.x};
+
+    if (idx >= N) {
+        return; 
+    }
+
+    C[idx] = A[idx] - B[idx];
+}
+
+extern "C" void solve(const float* A, const float* B, float* C, int N) {
+    int threads = 256;
+    int blocks = (N + threads - 1) / threads;
+    vector_sub<<<blocks, threads>>>(A, B, C, N);
+    cudaDeviceSynchronize();
+}
